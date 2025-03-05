51f6ed5c2ab207673ac1a9ddad0d25d1ab0939f6
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2025-03-05 08:34:32 +0100

    sulfura Test

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -42,4 +42,30 @@ class GildedRoseTest {
         assertEquals(1, app.items[0].sellIn);
     }
 
+    @Test
+    void SulfurasQualityDoesNotChange() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 2, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(20, app.items[0].quality);
+        assertEquals(2, app.items[0].sellIn);
+    }
+
+    @Test
+    void SulfurasQualityDoesNotChangeWithSellInZero() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(20, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+    }
+
+    @Test
+    void SulfurasQualityDoesNotChangeWithSellInNegative() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -1, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(20, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
 }
