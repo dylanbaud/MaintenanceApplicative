d79759eb1b4e86d42e06a4267f58646ee200705e
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2025-03-05 08:33:11 +0100

    agedBrie Test

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void AgedBrieQualityIncreases() {
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(11, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
+    }
+
+    @Test
+    void AgedBrieQualityIncreasesWithSellInZero() {
+        Item[] items = new Item[]{new Item("Aged Brie", 0, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(12, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void AgedBrieQualityIncreasesWithSellInNegative() {
+        Item[] items = new Item[]{new Item("Aged Brie", -1, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(12, app.items[0].quality);
+        assertEquals(-2, app.items[0].sellIn);
+    }
+
+    @Test
+    void AgedBrieQualityIncreasesWithFullQuality() {
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
     }
 
 }
