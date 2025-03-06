dd2a5ff35e0021480de0c85d20ff4c539ce22f47
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2025-03-06 10:28:49 +0100

    refactor switch

--- TriviaGame/src/main/java/trivia/Game.java
+++ TriviaGame/src/main/java/trivia/Game.java
@@ -88,16 +88,10 @@ public class Game implements IGame {
     private String currentCategory() {
         switch ((currentPlayer.getPlace() - 1)%4) {
             case 0:
-            case 4:
-            case 8:
                 return "Pop";
             case 1:
-            case 5:
-            case 9:
                 return "Science";
             case 2:
-            case 6:
-            case 10:
                 return "Sports";
             default:
                 return "Rock";
