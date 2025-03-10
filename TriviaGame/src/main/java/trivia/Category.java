eb0064da16620c42d6a671d8104f98706a1b1970
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2025-03-10 15:39:43 +0100

    Ajouter Géographie

--- TriviaGame/src/main/java/trivia/Category.java
+++ TriviaGame/src/main/java/trivia/Category.java
@@ -1,15 +1,15 @@
 package trivia;
 
 public enum Category {
-    POP("Pop"), SCIENCE("Science"), SPORTS("Sports"), ROCK("Rock");
+    POP("Pop"), SCIENCE("Science"), SPORTS("Sports"), ROCK("Rock"), GEOGRAPHIE("Géographie");
 
     private final String stringValue;
 
-    Category(final String s){
+    Category(final String s) {
         this.stringValue = s;
     }
 
-    public String toString(){
+    public String toString() {
         return this.stringValue;
     }
 }
